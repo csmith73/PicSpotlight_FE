cad0b0d51d3286fa50dbdb4a3ef9d340265dd2b2
Carter <cartersmith73@gmail.com>
2020-06-21 20:54:05 -0400

    host='0.0.0.0'

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask, render_template, request, send_file
 import os
 import requests
+import logging
 
 from PIL import Image
 import io
 
 app = Flask(__name__)
+
+gunicorn_error_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers.extend(gunicorn_error_logger.handlers)
+app.logger.setLevel(logging.DEBUG)
+app.logger.debug('this will show in the log')
 
 @app.route('/')
 def index():
